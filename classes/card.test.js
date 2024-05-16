944778bb400502358b6935570c1b530a9154d551
Carlos Adriano Miranda <cadriano.miranda@gmail.com>
2024-05-16 11:13:32 -0300

    test: card string and value

--- classes/card.test.js
+++ classes/card.test.js
@@ -33,4 +33,45 @@ describe("Card", () => {
     card.setIsHidden(true);
     expect(card.getIsHidden()).toBe(true);
   });
+
+  it("should return a string representation of the card", () => {
+    expect(card.toString()).toBe("Spades of Ace");
+  });
+
+  it("should return a string representation of the card when hidden", () => {
+    card.setIsHidden(true);
+    expect(card.toString()).toBe("Hidden");
+  });
+
+  it("should return the card value for Ace, King, Queen and Jack", () => {
+    card.setRank("Ace");
+    expect(card.getValue()).toBe(11);
+    card.setRank("King");
+    expect(card.getValue()).toBe(10);
+    card.setRank("Queen");
+    expect(card.getValue()).toBe(10);
+    card.setRank("Jack");
+    expect(card.getValue()).toBe(10);
+  });
+
+  it("should return the card value for 2-10", () => {
+    card.setRank("2");
+    expect(card.getValue()).toBe(2);
+    card.setRank("3");
+    expect(card.getValue()).toBe(3);
+    card.setRank("4");
+    expect(card.getValue()).toBe(4);
+    card.setRank("5");
+    expect(card.getValue()).toBe(5);
+    card.setRank("6");
+    expect(card.getValue()).toBe(6);
+    card.setRank("7");
+    expect(card.getValue()).toBe(7);
+    card.setRank("8");
+    expect(card.getValue()).toBe(8);
+    card.setRank("9");
+    expect(card.getValue()).toBe(9);
+    card.setRank("10");
+    expect(card.getValue()).toBe(10);
+  });
 });
