1e0ca5b0b0326a6e21de53e544aec45fbd20f5d1
Carlos Adriano Miranda <cadriano.miranda@gmail.com>
2024-05-16 09:57:41 -0300

    chore: fix test setup

--- utils/test.js
+++ utils/test.js
@@ -1,11 +1,18 @@
 import { JSDOM } from "jsdom";
-import fs from "fs";
 import path from "path";
 import { URL } from "url";
 
-const __dirname = new URL(".", import.meta.url).pathname;
-const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
+const __dirname = new URL("../", import.meta.url).pathname;
 
-export const getContainer = () => {
-  return new JSDOM(html, { runScripts: "dangerously" }).window.document.body;
+export const getContainer = async () => {
+  const dom = await JSDOM.fromFile(path.resolve(__dirname, "./index.html"), {
+    runScripts: "dangerously",
+    resources: "usable",
+  });
+
+  return new Promise((resolve) => {
+    dom.window.addEventListener("load", () => {
+      resolve(dom.window.document.body);
+    });
+  });
 };
