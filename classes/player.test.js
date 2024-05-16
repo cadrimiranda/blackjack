9151187b30f5b7e4c34649d2928d63a7646ef6a7
Carlos Adriano Miranda <cadriano.miranda@gmail.com>
2024-05-16 11:01:03 -0300

    test: calculate player hand

--- classes/player.test.js
+++ classes/player.test.js
@@ -27,4 +27,48 @@ describe("Player", () => {
     expect(card2).toBeInstanceOf(Card);
     expect(card2.toString()).toBe("King of Hearts");
   });
+
+  it("should return player black jack with Ace and King", () => {
+    player.addCard(new Card("Ace", "Spades"));
+    player.addCard(new Card("King", "Hearts"));
+    expect(player.getHandTotal()).toBe(21);
+  });
+
+  it("should return player black jack with Ace and Queen", () => {
+    player.addCard(new Card("Ace", "Spades"));
+    player.addCard(new Card("Queen", "Hearts"));
+    expect(player.getHandTotal()).toBe(21);
+  });
+
+  it("should return player black jack with Ace and Jack", () => {
+    player.addCard(new Card("Ace", "Spades"));
+    player.addCard(new Card("Jack", "Hearts"));
+    expect(player.getHandTotal()).toBe(21);
+  });
+
+  it("should return player black jack with Ace and 10", () => {
+    player.addCard(new Card("Ace", "Spades"));
+    player.addCard(new Card("10", "Hearts"));
+    expect(player.getHandTotal()).toBe(21);
+  });
+
+  it("should return ace with 1 point when player hand would bust", () => {
+    player.addCard(new Card("Ace", "Spades"));
+    player.addCard(new Card("9", "Hearts"));
+    player.addCard(new Card("9", "Spades"));
+    expect(player.getHandTotal()).toBe(19);
+  });
+
+  it("should return ace with 11 points when player hand would not bust", () => {
+    player.addCard(new Card("Ace", "Spades"));
+    player.addCard(new Card("9", "Hearts"));
+    expect(player.getHandTotal()).toBe(20);
+  });
+
+  it("should return bust when player hand is over 21", () => {
+    player.addCard(new Card("10", "Spades"));
+    player.addCard(new Card("10", "Hearts"));
+    player.addCard(new Card("2", "Diamonds"));
+    expect(player.getHandTotal()).toBe(22);
+  });
 });
